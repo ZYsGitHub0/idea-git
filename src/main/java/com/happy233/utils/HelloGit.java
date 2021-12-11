02b90ad308093dfa6a27ff1f1739eb60120849e3
zhangyi <709608032@qq.com>
2021-12-11 21:19:34 +0800

    #bug fix

--- src/main/java/com/happy233/utils/HelloGit.java
+++ src/main/java/com/happy233/utils/HelloGit.java
@@ -5,5 +5,6 @@ public class HelloGit {
         System.out.println("Hello Git!");
         System.out.println("change one ....(already merge)");
         System.out.println("something change in #coder1");
+        System.out.println("解决bug...");
     }
 }
