00e399cc02ac0bafa6463339a2702052bcddc4a6
zhangyi <709608032@qq.com>
2021-12-11 21:05:06 +0800

    #coder1制造冲突

--- src/main/java/com/happy233/utils/HelloGit.java
+++ src/main/java/com/happy233/utils/HelloGit.java
@@ -3,7 +3,7 @@ package com.happy233.utils;
 public class HelloGit {
     public static void main(String[] args) {
         System.out.println("Hello Git!");
-        System.out.println("change one ....");
+        System.out.println("change one ....(Make it conflict with #coder1)");
         System.out.println("something change in #coder1");
     }
 }
